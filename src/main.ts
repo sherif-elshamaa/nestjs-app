3e1cf72f5ed4531d4757724aaffdf2d2bd093997
sherif-elshamaa <sherif.elshamaaa@gmail.com>
2025-04-06 21:53:44 +0200

    add env.example

--- src/main.ts
+++ src/main.ts
@@ -24,8 +24,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/v1', app, document);
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 5000);
 
-  Logger.log('App is running on port ' + (process.env.PORT ?? 3000));
+  Logger.log('App is running on port ' + (process.env.PORT ?? 5000));
 }
 bootstrap();
